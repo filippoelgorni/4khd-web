f7907050c97cb6db5c14dc1df34c00a9a48b7200
filippoelgorni <f.elgorni@alice.it>
2023-10-14 20:28:07 +0200

    draft carousel, link to cyclobrowing

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,30 @@
+import { title } from "process";
+import "./style.scss";
+
+export function Carousel({
+  titleImageUrl,
+  title,
+  images,
+  description,
+  credits,
+  year,
+}: {
+    titleImageUrl: string,
+    title: string,
+    images: string[];
+    description: string;
+    credits: string;
+    year: number;
+}) {
+  return <div className="carousel">
+    <div className="carousel-content">
+    {images.map(image=> <img src={image} className="carousel-image" alt="" />)}
+    <div className='carousel-description'>{description}</div>
+    <div  className="carousel-footer">
+    <div >{title}</div>
+    <div >{credits}</div>
+    <div >{year}</div>
+    </div>
+    </div>
+  </div>
+}
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,37 @@
 import { Link } from "react-router-dom";
-import {useAllPrismicDocumentsByType} from '@prismicio/react'
+import { useAllPrismicDocumentsByType } from "@prismicio/react";
+import { Carousel } from "./Carousel";
 
 import "./style.scss";
 
-
-
 function Home() {
+  const [projectsRaw] = useAllPrismicDocumentsByType("project");
+  const projects = projectsRaw?.map((p) => p.data);
 
-  const [projectsRaw] = useAllPrismicDocumentsByType('project')
-  const projects = projectsRaw?.map(p=>p.data)
-
+  const selectedProject = projects?.[1];
+  console.log("✌️selectedProject --->", selectedProject);
 
   return (
     <div className="home">
-      <Link to="/cyclobrowsing">cyclobrowsing</Link>
+      <Link to="/cyclobrowsing" className="flying-igor">
+      <div className="where-are-you">Ma dove sei frate?</div>
+      <img src="./assets/igor.png" alt='igor' className="flying-igor-image"/>
+      </Link>
       <div className="project-titles">
-      {projects?.map(p=><img src={p.title_svg.url} className="project-title" alt=''/>)}
+        {projects?.map((p) => (
+          <img src={p.title_svg.url} className="project-title" alt="" />
+        ))}
       </div>
+      {selectedProject && (
+        <Carousel
+          credits={selectedProject.authors[0].text}
+          images={selectedProject.images.map((image: any) => image.image.url)}
+          description={selectedProject.description.flatMap((d:any)=>d.text)}
+          titleImageUrl={selectedProject.title_svg.url}
+          title={selectedProject.project_name[0].text}
+          year={selectedProject.year}
+        />
+      )}
       <footer className="footer">
         <div className="footer-content">
           4KHD is a group of people and an ever-changing idea. We exist in
